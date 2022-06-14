d4f5d38db1fa3c8b9ea5517148ba466f2d4076ce
Ekaterina Zhdanova <zhdanova.ekaterina.95@yandex.ru>
2022-06-14 21:25:47 +0300

    ver 1.0

--- metrika.py
+++ metrika.py
@@ -4,8 +4,27 @@ import pandas as pd
 import config
 import time
 from datetime import date, timedelta
+import datetime
 import logging
 import gspread
+import sys
+
+def get_needed_data(worksheet):
+    '''Получает объект листа Google таблиц, из которого нужно получить данные. Возвращает кортеж из двух дат для выгрузки в формате строки. Первую считает как следующую за последней, которая уже есть в таблице с данными. Последнюю - вчера. Если период выгрузки отрицательный, генерирует SystemExit.'''
+
+    values_list = worksheet.col_values(1)
+    date1 = values_list[-1]
+    date1 = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
+    date1 = date1 + timedelta(days=1)
+    date2 = date.today() - timedelta(days=1)       # вчера
+    date_diff = (date2 - date1).days
+    if date_diff < 0:
+        sys.exit('Нет новых данных для загрузки. Попробуйте завтра.')
+    else:
+        date1 = str(date1)
+        date2 = str(date2)
+        dates = (date1, date2)
+        return dates
 
 def import_data(api_request, params):
     '''Получает данные из Yandex.Metrika API. Возвращает массив результатов'''
@@ -27,14 +46,7 @@ def parse_json(result):
             value.append(result[i]["metrics"][m])
         values.append(value)
     return values
-
-def to_google_sheets(data):
-    '''Выгрузка данных в Google Таблицы. Работает, если необходимо добавить меньше 100 строк!'''
-    gc = gspread.service_account(filename='google_key.json')
-    sheet = gc.open_by_key(config.sheet)
-    worksheet = sheet.worksheet(config.worksheet)
-    worksheet.append_rows(data)
-
+  
 def main():
     start_time = time.time()
 
@@ -42,11 +54,14 @@ def main():
     ACCESS_TOKEN = config.token
     METRIC_IDS = config.counterId
 
-    # Задание периода выгрузки
-    # TODO: задавать период со следующего дня после того, который последний на листе по вчера; добавить выгрузку последней даты и проверку, что timedelta положительна (иначе, что последняя дата - не вчера)
-    date1 = str(date.today() - timedelta(days=1))       # вчера
-    date2 = str(date.today() - timedelta(days=1))       # вчера
+    # Получение листа Google таблиц для работы
+    gc = gspread.service_account(filename='google_key.json')
+    sheet = gc.open_by_key(config.sheet)
+    worksheet = sheet.worksheet(config.worksheet)
 
+    # Получение начальной и конечной даты диапазона загрузки данных
+    dates = get_needed_data(worksheet)
+    
     #Параметры запроса для библиотеки tapi_yandex_metrika
     api = YandexMetrikaStats(
         access_token=ACCESS_TOKEN, 
@@ -56,15 +71,15 @@ def main():
         ids = METRIC_IDS,
         metrics = "ym:s:visits,ym:s:users,ym:s:goal49436773reaches,ym:s:goal47702074reaches",
         dimensions = "ym:s:date,ym:s:lastsignTrafficSource,ym:s:lastsignSourceEngine,ym:s:UTMCampaign",
-        date1 = date1,
-        date2 = date2,
+        date1 = dates[0],
+        date2 = dates[1],
         sort = "ym:s:date",
         accuracy="full",
         limit = 2000)
 
     result = import_data(api, params)
     values = parse_json(result)
-    to_google_sheets(values)
+    worksheet.append_rows(values)
     
     end_time = time.time()
     total_time = round((end_time - start_time), 3)
